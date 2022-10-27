5401bb4c47cc4776ea039bf288b5a1011bfef1ec
Eh.Emon <eh.emon3059@gmail.com>
2022-10-27 20:55:00 +0600

    courses data added

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
+
+app.get('/', (req,res)=> {
+    res.send('News Api Running')
+});
+
+app.get('/courses', (req,res) => {
+    res.send(courses)
+})
+
+
+app.listen(port , () => {
+    console.log('Course api running', port);
+})
