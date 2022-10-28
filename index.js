5672a904a64ca8dae273a2d50fbbdcc54f067b69
Eh.Emon <eh.emon3059@gmail.com>
2022-10-28 23:06:03 +0600

    courses details data setup

--- index.js
+++ index.js
@@ -14,4 +14,10 @@ app.get('/', (req,res)=> {
 
 app.get('/courses', (req,res) => {
     res.send(courses)
+});
+
+app.get('/courses/:id', (req,res) => {
+    const id = req.params.id;
+    const data = courses.find(cours => cours.id == id)
+    res.send(data);
 })
